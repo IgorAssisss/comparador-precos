1e2ed176429c9f06f083223c447b46b202c83c88
IgorAssisss <142446030+IgorAssisss@users.noreply.github.com>
2025-03-01 06:13:27 -0300

    Update coletar_ofertas.py

--- coletar_ofertas.py
+++ coletar_ofertas.py
@@ -7,6 +7,7 @@ from config import supabase
 
 def setup_driver():
     options = webdriver.ChromeOptions()
+    options.add_argument("--headless")     
     options.add_argument('--disable-gpu')
     options.add_argument('--window-size=1920x1080')
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
